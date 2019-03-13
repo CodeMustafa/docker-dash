cfaedcb8b84c7b022d875052aaae76ab4a217abe
Mustafa Abdelwahab <mustafaabdalwahab@gmail.com>
2019-03-13 17:44:23 +0400

    adding the Index page

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const Docker = require('dockerode');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Docker!'));
+var docker = new Docker({
+  socketPath: '/var/run/docker.sock'
+});
+
+app.get('/', (req, res) => {
+	// filter by labels
+	var opts = {
+		"limit": 3,
+		"filters": '{"label": []}'
+	};
+	
+	// maps are also supported (** requires docker-modem 0.3+ **)
+	opts["filters"] = {
+		"label": []
+	};
+	docker.listImages(opts, function(err, images) {
+		var images_names = '';
+		var images_ids = '';
+
+		images.forEach(function (imageInfo) {
+			images_names+='</br>'+imageInfo.RepoTags[0];
+			images_ids += '</br>'+imageInfo.Id;
+		});
+		res.send('No of System Images : ' + images.length + '</br></hr></br> Images Names : ' + images_names +'</br></hr></br> Images Ids : '+images_ids);
+	});
+});
 
 app.listen(port, () => console.log(`Docker-Dash is listening on port ${port}!`));
